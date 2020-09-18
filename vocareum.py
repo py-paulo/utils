e89c0b5985647e8dd22d3759da8de561bcd430cb
py-paulo <paulo.rb.beserra@gmail.com>
2020-09-17 22:52:07 -0300

    docs: code rate pylint plus

--- vocareum.py
+++ vocareum.py
@@ -63,7 +63,8 @@ URL_LOGIN_GET = URL + '/home/login.php?code=&e=Please%20enter%20a%20valid%20emai
 URL_GET_AWS_ACCESS = URL + '/util/vcput.php?a=getaws&nores=1&stepid=14335&mode=s&type=0&vockey=%s'
 URL_HOME_PAGE = URL + '/main/main.php?m=editor&nav=1&asnid=14334&stepid=14335'
 
-wl = ['PHPSESSID', 'logintoken', 'tokenExpire', 'usertoken', 'userid', 'userassignment', 'domain_latestWebProxy']
+wl = ['PHPSESSID', 'logintoken', 'tokenExpire', 'usertoken', 'userid', 'userassignment',
+      'domain_latestWebProxy']
 
 headers = {
     'Host': 'labs.vocareum.com',
@@ -74,14 +75,22 @@ headers = {
 }
 
 
-def try_login(mail, password):
-    rq = requests.get(url=URL_LOGIN_GET, headers=headers)
-    cookies = rq.headers.get('Set-Cookie')
+def try_login(mail: str, password: str) -> None:
+    """login in to the Vocarium platform, then immediately
+    post to get the temporary keys for aws CLI access
+
+    Args:
+        mail (str): your email address
+        password (str): password for login
+    """
+    req = requests.get(url=URL_LOGIN_GET, headers=headers)
+    cookies = req.headers.get('Set-Cookie')
     cfduid = cookies.split(';')[0].split('=')
     cookie = {cfduid[0]: cfduid[1]}
 
     headers['Content-Type'] = 'application/x-www-form-urlencoded'
-    headers['Referer'] = 'https://labs.vocareum.com/home/login.php?code=&e=Please%20enter%20a%20valid%20email%20address'
+    headers['Referer'] = (r'https://labs.vocareum.com/home/login.php?code=&e=Please%20enter%20a%'
+                          r'20valid%20email%20address')
     headers['DNT'] = '1'
 
     datapost = 'sender=home&loginid=%s&passwd=%s' % (mail, password)
@@ -91,9 +100,10 @@ def try_login(mail, password):
     headers['Connection'] = 'Close'
     headers['Cookie'] = '='.join(cfduid)
 
-    rq = requests.post(url=URL_LOGIN, data=datapost, headers=headers, cookies=cookie, allow_redirects=False)
+    req = requests.post(url=URL_LOGIN, data=datapost, headers=headers, cookies=cookie,
+                       allow_redirects=False)
 
-    cookies = rq.headers.get('Set-Cookie')
+    cookies = req.headers.get('Set-Cookie')
     vockey = re.findall(r'\w{32}', cookies)[0]
 
     headers.pop('Content-Type')
@@ -103,45 +113,45 @@ def try_login(mail, password):
     new_cookies = {cfduid[0]: cfduid[1]}
 
     for item in wl:
-        for ck in cookies.split(';'):
-            if item in ck:
-                tmp = ck.replace('Secure, ', '').split('=')
+        for _cookie in cookies.split(';'):
+            if item in _cookie:
+                tmp = _cookie.replace('Secure, ', '').split('=')
                 new_cookies[tmp[0]] = tmp[1]
 
-    rq = requests.get(url=URL_HOME_PAGE, headers=headers, cookies=new_cookies)
+    req = requests.get(url=URL_HOME_PAGE, headers=headers, cookies=new_cookies)
 
     proxy = re.findall(
-        r'https://proxy.vocareum.com/hostip/(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}/vocproxy/[\w|.]{10,70}',
-        rq.text)
-    proxy = 'https://proxy.vocareum.com/hostip/172.31.18.122:5000/vocproxy/72448074974689574689514335015eee0b738955c4' \
-            '.96014152' if len(proxy) == 0 else proxy[0]
+        r'https://proxy.vocareum.com/hostip/(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{1,5}'
+        r'/vocproxy/[\w|.]{10,70}',
+        req.text)
+    proxy = 'https://proxy.vocareum.com/hostip/172.31.18.122:5000/vocproxy/7244807497468957'\
+            '4689514335015eee0b738955c4.96014152' if len(proxy) == 0 else proxy[0]
 
     new_cookies['domain_latestWebProxy'] = proxy
 
     headers['Referer'] = 'https://labs.vocareum.com/main/main.php'
     headers['X-Requested-With'] = 'XMLHttpRequest'
 
-    rq = requests.get(url=URL_GET_AWS_ACCESS % vockey, headers=headers, cookies=new_cookies)
+    req = requests.get(url=URL_GET_AWS_ACCESS % vockey, headers=headers, cookies=new_cookies)
 
-    aws_credentials = rq.text
+    aws_credentials = req.text
 
     aws_access_key_id = re.findall(r'aws_access_key_id=(.*)', aws_credentials)[0]
     aws_secret_access_key = re.findall(r'aws_secret_access_key=(.*)', aws_credentials)[0]
     aws_session_token = re.findall(r'aws_session_token=(.*)', aws_credentials)[0]
 
-    aws_write_lines = ['aws_access_key_id=%s' % aws_access_key_id, 'aws_secret_access_key=%s' % aws_secret_access_key,
-                       'aws_session_token=%s' % aws_session_token]
+    aws_write_lines = ['aws_access_key_id=%s' % aws_access_key_id, 'aws_secret_access_key=%s' % \
+                       aws_secret_access_key, 'aws_session_token=%s' % aws_session_token]
 
     for line in aws_write_lines:
         print(line)
 
     os.remove(PATH_AWS_CREDENTIALS)
 
-    with open(PATH_AWS_CREDENTIALS, 'w+') as fp:
-        fp.write('[default]\n')
-
+    with open(PATH_AWS_CREDENTIALS, 'w+') as fip:
+        fip.write('[default]\n')
         for line in aws_write_lines:
-            fp.write('%s\n' % line)
+            fip.write('%s\n' % line)
 
 
 if __name__ == '__main__':
@@ -155,4 +165,3 @@ if __name__ == '__main__':
         try_login(sys.argv[1], passwd)
     else:
         pass
-
